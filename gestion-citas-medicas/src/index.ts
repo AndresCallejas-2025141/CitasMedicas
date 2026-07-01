aaa4232c6040958cf98eae009159ff10579f2ad6
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-07-01 08:45:35 -0600

    add: Se agrego nuevo codigo para verificar funcionamiento

--- gestion-citas-medicas/src/index.ts
+++ gestion-citas-medicas/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Gestión de Citas Médicas");
+import { conexion } from "./config/conexion";
+
+async function iniciar() {
+    try {
+        const connection = await conexion.getConnection();
+
+        console.log("✅ Conexión a MySQL establecida correctamente.");
+
+        connection.release();
+    } catch (error) {
+        console.error("❌ Error al conectar con la base de datos:");
+        console.error(error);
+    }
+}
+
+iniciar();
