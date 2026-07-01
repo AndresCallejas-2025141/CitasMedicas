3a31b098cce8f1f0b9f0d3b79029b5ba8c874b3c
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-07-01 10:26:02 -0600

    add: Se agrego doctorService con codigo

---
+++ gestion-citas-medicas/src/services/doctorService.ts
@@ -0,0 +1,50 @@
+import { conexion } from "../config/database";
+import { Doctor } from "../models/doctor";
+
+export class DoctorService {
+
+    async listar(): Promise<any> {
+        const [rows] = await conexion.query("CALL listar_doctor()");
+        return rows;
+    }
+
+    async buscarPorId(id: number): Promise<any> {
+        const [rows] = await conexion.query(
+            "CALL buscar_doctor_id(?)",
+            [id]
+        );
+
+        return rows;
+    }
+
+    async agregar(doctor: Doctor): Promise<void> {
+        await conexion.query(
+            "CALL agregar_doctor(?,?,?)",
+            [
+                doctor.nombre,
+                doctor.especialidadId,
+                doctor.idDoctor
+            ]
+        );
+    }
+
+    async editar(doctor: Doctor): Promise<void> {
+        await conexion.query(
+            "CALL editar_doctor(?,?,?,?)",
+            [
+                doctor.idDoctor,
+                doctor.nombre,
+                doctor.especialidadId,
+                doctor.idDoctor
+            ]
+        );
+    }
+
+    async eliminar(id: number): Promise<void> {
+        await conexion.query(
+            "CALL eliminar_doctor(?)",
+            [id]
+        );
+    }
+
+}
