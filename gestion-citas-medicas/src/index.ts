f02ed347f698b0cf371805be1ba3405809d02e9d
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-07-01 11:41:53 -0600

    test: Se modifico para que cargue el menu.ts

--- gestion-citas-medicas/src/index.ts
+++ gestion-citas-medicas/src/index.ts
@@ -1,16 +1,3 @@
-import { conexion } from "./config/database";
+import { mostrarMenu } from "./menu/menu";
 
-async function iniciar() {
-    try {
-        const connection = await conexion.getConnection();
-
-        console.log("✅ Conexión a MySQL establecida correctamente.");
-
-        connection.release();
-    } catch (error) {
-        console.error("❌ Error al conectar a la base de datos:");
-        console.error(error);
-    }
-}
-
-iniciar();
+mostrarMenu();
