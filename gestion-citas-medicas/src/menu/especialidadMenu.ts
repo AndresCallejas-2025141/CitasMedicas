88eb4bc618ffb530a1f2c66b3b68aeb02820334a
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-07-22 11:19:04 -0600

    test: Se agregaron validaciones para especialidadMenu

--- gestion-citas-medicas/src/menu/especialidadMenu.ts
+++ gestion-citas-medicas/src/menu/especialidadMenu.ts
@@ -5,6 +5,7 @@ import { mostrarMenu } from "./menu";
 
 const especialidadService = new EspecialidadService();
 
+
 export function menuEspecialidades(): void {
 
     console.clear();
@@ -19,8 +20,8 @@ export function menuEspecialidades(): void {
     console.log("5. Eliminar especialidad");
     console.log("0. Regresar");
     console.log("====================================");
-
     rl.question("Seleccione una opción: ", async (opcion) => {
+
 
         switch (opcion) {
 
@@ -52,6 +53,7 @@ export function menuEspecialidades(): void {
                 console.log("Opción inválida.");
                 setTimeout(menuEspecialidades, 1500);
                 break;
+
         }
 
     });
@@ -62,18 +64,33 @@ function agregarEspecialidad(): void {
 
     console.clear();
 
+
     rl.question("Nombre de la especialidad: ", async (nombre) => {
 
+
+        if (nombre.trim() === "") {
+
+            console.log("Error: No se permiten campos vacíos.");
+            volverMenuEspecialidades();
+            return;
+
+        }
+
+
         try {
+
 
             const especialidad = new Especialidad(
                 0,
                 nombre
             );
 
+
             await especialidadService.agregar(especialidad);
 
+
             console.log("Especialidad agregada correctamente.");
+
 
         } catch (error) {
 
@@ -81,7 +98,9 @@ function agregarEspecialidad(): void {
 
         }
 
+
         volverMenuEspecialidades();
+
 
     });
 
@@ -91,17 +110,20 @@ async function listarEspecialidades(): Promise<void> {
 
     console.clear();
 
+
     try {
 
         const resultado = await especialidadService.listar();
 
         console.table(resultado[0]);
 
+
     } catch (error) {
 
         console.error(error);
 
     }
+
 
     volverMenuEspecialidades();
 
@@ -111,13 +133,44 @@ function buscarEspecialidad(): void {
 
     console.clear();
 
+
     rl.question("Ingrese el ID de la especialidad: ", async (id) => {
+
+
+        if (id.trim() === "") {
+
+            console.log("Error: El ID no puede estar vacío.");
+            volverMenuEspecialidades();
+            return;
+
+        }
+
+
+        if (isNaN(Number(id))) {
+
+            console.log("Error: El ID debe ser numérico.");
+            volverMenuEspecialidades();
+            return;
+
+        }
+
 
         try {
 
+
             const resultado = await especialidadService.buscarPorId(Number(id));
 
-            console.table(resultado[0]);
+
+            if (resultado[0].length === 0) {
+
+                console.log("Error: Especialidad no encontrada.");
+
+            } else {
+
+                console.table(resultado[0]);
+
+            }
+
 
         } catch (error) {
 
@@ -125,7 +178,9 @@ function buscarEspecialidad(): void {
 
         }
 
+
         volverMenuEspecialidades();
+
 
     });
 
@@ -135,20 +190,48 @@ function editarEspecialidad(): void {
 
     console.clear();
 
+
     rl.question("ID: ", (id) => {
+
 
         rl.question("Nombre: ", async (nombre) => {
 
+
+            if (
+                id.trim() === "" ||
+                nombre.trim() === ""
+            ) {
+
+                console.log("Error: No se permiten campos vacíos.");
+                volverMenuEspecialidades();
+                return;
+
+            }
+
+
+            if (isNaN(Number(id))) {
+
+                console.log("Error: El ID debe ser numérico.");
+                volverMenuEspecialidades();
+                return;
+
+            }
+
+
             try {
+
 
                 const especialidad = new Especialidad(
                     Number(id),
                     nombre
                 );
 
+
                 await especialidadService.editar(especialidad);
 
+
                 console.log("Especialidad actualizada correctamente.");
+
 
             } catch (error) {
 
@@ -156,9 +239,12 @@ function editarEspecialidad(): void {
 
             }
 
+
             volverMenuEspecialidades();
 
+
         });
+
 
     });
 
@@ -168,13 +254,36 @@ function eliminarEspecialidad(): void {
 
     console.clear();
 
+
     rl.question("Ingrese el ID de la especialidad: ", async (id) => {
+
+
+        if (id.trim() === "") {
+
+            console.log("Error: El ID no puede estar vacío.");
+            volverMenuEspecialidades();
+            return;
+
+        }
+
+
+        if (isNaN(Number(id))) {
+
+            console.log("Error: El ID debe ser numérico.");
+            volverMenuEspecialidades();
+            return;
+
+        }
+
 
         try {
 
+
             await especialidadService.eliminar(Number(id));
 
+
             console.log("Especialidad eliminada correctamente.");
+
 
         } catch (error) {
 
@@ -182,7 +291,9 @@ function eliminarEspecialidad(): void {
 
         }
 
+
         volverMenuEspecialidades();
+
 
     });
 
